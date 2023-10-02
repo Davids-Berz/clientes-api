b31c903af4c5d60e1ae434f06f0eb7e2d1ffb358
DavidBerz <davids.berz@gmail.com>
2023-10-02 06:24:56 -0600

    cors fixed

--- src/main/java/com/clientes/comtrollers/ClienteRestController.java
+++ src/main/java/com/clientes/comtrollers/ClienteRestController.java
@@ -3,12 +3,16 @@ package com.clientes.comtrollers;
 import com.clientes.models.entity.Cliente;
 import com.clientes.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = {"http://localhost:4200"})
 public class ClienteRestController {
 
     private IClienteService clienteService;
