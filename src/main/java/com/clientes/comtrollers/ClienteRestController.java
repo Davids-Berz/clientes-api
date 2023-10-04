e6a03158c645efa87c04258a199e92ccaa1ae879
DavidBerz <davids.berz@gmail.com>
2023-10-03 20:06:25 -0600

    manejo de error en findById

--- src/main/java/com/clientes/comtrollers/ClienteRestController.java
+++ src/main/java/com/clientes/comtrollers/ClienteRestController.java
@@ -3,11 +3,15 @@ package com.clientes.comtrollers;
 import com.clientes.models.entity.Cliente;
 import com.clientes.models.services.IClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -27,8 +31,23 @@ public class ClienteRestController {
     }
 
     @GetMapping("/clientes/{id}")
-    public Cliente findById(@PathVariable Long id) {
-        return clienteService.findById(id);
+    public ResponseEntity<?> findById(@PathVariable Long id) {
+        Map<String, String> response = new HashMap<>();
+        Cliente cliente = null;
+
+        try {
+            cliente = clienteService.findById(id);
+        } catch (DataAccessException e) {
+            response.put("mensaje", "Error al consultar la base de datos: " + e.getMostSpecificCause().getMessage());
+            return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+
+        if (cliente == null) {
+            response.put("mensaje", "El cliente ID: " + id.toString() + " no existe!");
+            return new ResponseEntity<>(cliente, HttpStatus.NOT_FOUND);
+        }
+
+        return new ResponseEntity<>(cliente, HttpStatus.OK);
     }
 
     @PostMapping("/clientes")
@@ -39,7 +58,7 @@ public class ClienteRestController {
 
     @PutMapping("/clientes/{id}")
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente update(@RequestBody Cliente cliente,@PathVariable Long id) {
+    public Cliente update(@RequestBody Cliente cliente, @PathVariable Long id) {
         Cliente clienteActual = clienteService.findById(id);
         clienteActual.setNombre(cliente.getNombre());
         clienteActual.setApellido(cliente.getApellido());
